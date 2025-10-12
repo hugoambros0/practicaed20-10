51595cb67a4fa42473fc70d3974dd2d124d13f33
Miquel <101179847+mpetrus1@users.noreply.github.com>
2025-10-12 19:21:41 +0200

    Create InfoPersona.java

---
+++ InfoPersona.java
@@ -0,0 +1,16 @@
+public class InfoPersona {
+
+    public String nom;
+    public int edat;
+
+    public InfoPersona(String nom, int edat) {
+        this.nom = nom;
+        this.edat = edat;
+    }
+
+    public void mostrarInformacio() {
+        System.out.println("Nom: " + nom + ", edat: " + edat);
+    }
+
+    // TODO: Afegeix aquí una nova funció segons les instruccions del README
+}
