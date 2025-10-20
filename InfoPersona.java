aaac39d2b38111c4176fef980dc38d0f9a455e1f
Hugo <hambros20696@iesjoanramis.org>
2025-10-20 16:17:02 +0200

    Afegida  funció felicitarAniversari amb documentació JavaDoc

--- InfoPersona.java
+++ InfoPersona.java
@@ -1,16 +1,42 @@
+/**
+ * <p>Informacio d'una persona</p>
+ * 
+ * @author Hugo
+ * @version 1.0
+ * @since 1.0
+ * @see <a href="https://google.com">Google</a>
+ */
 public class InfoPersona {
-
-    public String nom;
+    /**
+     * Guardar nombre de la persona
+     */
+	public String nom;
+	/**
+	 * Guardar la edad de la persona
+	 */
     public int edat;
-
+    /**
+     * 
+     * @param nom Nom de la persona
+     * @param edat Edat de la persona
+     */
     public InfoPersona(String nom, int edat) {
         this.nom = nom;
         this.edat = edat;
     }
-
+    
+    /**
+     * Funcion para mostrar informacion de la persona
+     */
     public void mostrarInformacio() {
         System.out.println("Nom: " + nom + ", edat: " + edat);
     }
 
-    // TODO: Afegeix aquí una nova funció segons les instruccions.
+    /**
+     * Funcion para felicitar a la persona
+     */
+    public void felicitarAniversari() { 
+    	 edat++; 
+    	 System.out.println("Bon aniversari, " + nom + "! Ara tens " + edat + " anys."); 
+    }
 }
